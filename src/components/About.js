32e7812290f28277532f56b4228402d524106b50
ayushkumar-mnnit <ayushk.2125@gmail.com>
2023-11-23 02:02:47 +0530

    adding about page

---
+++ src/components/About.js
@@ -0,0 +1,209 @@
+import React from 'react'
+import './About.css'
+
+import { Navigate, useNavigate } from 'react-router-dom'
+
+
+
+const About = () => {
+  
+  return (
+    <>
+  
+   
+    <div className='wrapunderc'>
+    
+      <h1 className='wraphead'>We Provide</h1>
+     
+    <div className="ag-format-container">
+  <div className="ag-courses_box">
+    <div className="ag-courses_item">
+      <a href="#" className="ag-courses-item_link">
+        <div className="ag-courses-item_bg" />
+        <div className="ag-courses-item_title">
+          Complete Transaaction in different Currency
+          
+        </div>
+        
+      </a>
+    </div>
+    <div className="ag-courses_item">
+      <a href="#" className="ag-courses-item_link">
+        <div className="ag-courses-item_bg" />
+        <div className="ag-courses-item_title">
+       All kind of Bill managment Page 
+       with proper notification
+        </div>
+      
+      </a>
+    </div>
+    <div className="ag-courses_item">
+      <a href="#" className="ag-courses-item_link">
+        <div className="ag-courses-item_bg" />
+        <div className="ag-courses-item_title">
+            Chat page for all payment simplicity
+        
+        </div>
+       
+      </a>
+    </div>
+    <div className="ag-courses_item">
+      <a href="#" className="ag-courses-item_link">
+        <div className="ag-courses-item_bg" />
+        <div className="ag-courses-item_title">
+            for view issue we have proper dark and night mode 
+        </div>
+      
+      </a>
+    </div>
+    <div className="ag-courses_item">
+      <a href="#" className="ag-courses-item_link">
+        <div className="ag-courses-item_bg" />
+        <div className="ag-courses-item_title">
+            Your payment chart graph and other information
+        </div>
+       
+      </a>
+    </div>
+    <div className="ag-courses_item">
+      <a href="#" className="ag-courses-item_link">
+        <div className="ag-courses-item_bg" />
+        <div className="ag-courses-item_title">
+         Payment managment page for your simplicity
+        </div>
+      </a>
+    </div>
+    <div className="ag-courses_item">
+      <a href="#" className="ag-courses-item_link">
+        <div className="ag-courses-item_bg" />
+        <div className="ag-courses-item_title">
+         Payment managment page for your simplicity
+        </div>
+      </a>
+    </div>
+    <div className="ag-courses_item">
+      <a href="#" className="ag-courses-item_link">
+        <div className="ag-courses-item_bg" />
+        <div className="ag-courses-item_title">
+         Payment managment page for your simplicity
+        </div>
+      </a>
+    </div>
+    <div className="ag-courses_item">
+      <a href="#" className="ag-courses-item_link">
+        <div className="ag-courses-item_bg" />
+        <div className="ag-courses-item_title">
+         Payment managment page for your simplicity
+        </div>
+      </a>
+    </div>
+    <div className="ag-courses_item">
+      <a href="#" className="ag-courses-item_link">
+        <div className="ag-courses-item_bg" />
+        <div className="ag-courses-item_title">
+         Payment managment page for your simplicity
+        </div>
+      </a>
+    </div>
+    <div className="ag-courses_item">
+      <a href="#" className="ag-courses-item_link">
+        <div className="ag-courses-item_bg" />
+        <div className="ag-courses-item_title">
+         Payment managment page for your simplicity
+        </div>
+      </a>
+    </div>
+    <div className="ag-courses_item">
+      <a href="#" className="ag-courses-item_link">
+        <div className="ag-courses-item_bg" />
+        <div className="ag-courses-item_title">
+         Payment managment page for your simplicity
+        </div>
+      </a>
+    </div>
+    
+
+  
+   
+  </div>
+
+  
+
+</div>
+<h1 className='teamhead'>About creator</h1>
+<div className='combined'>
+<div className="card-container">
+
+  <img
+    className="round"
+    src="https://media.licdn.com/dms/image/D4E03AQGwExjVfOqE5Q/profile-displayphoto-shrink_100_100/0/1687794294297?e=1705536000&v=beta&t=VaDn7Zcc6dAO9N0JHGwVn6os_9sssuYDSCnyCMchbrY"
+    alt="user"
+  />
+  <h3>Ayush Kumar</h3>
+  <h6>Full Stack web developer</h6>
+  <p>
+  Explore our services and manage your transactions with ease. If you have any questions or need assistance, feel free to reach out to our support team.
+  </p>
+  <div className="buttons">
+    <button className="primary">Message</button>
+   
+  </div>
+  <div className="skills">
+    <h6>Skills</h6>
+    <ul>
+    <li>UI / UX</li>
+      <li>Front End Development</li>
+      <li>HTML</li>
+      <li>CSS</li>
+      <li>JavaScript</li>
+      <li>React</li>
+      <li>Node</li>
+      <li>Express.js</li>
+      <li>Mongodb</li>
+    </ul>
+  </div>
+</div>
+<div className="card-container">
+  
+  <img
+    className="round"
+    src="https://media.licdn.com/dms/image/D4D03AQEv-N_telX5iw/profile-displayphoto-shrink_100_100/0/1689699598791?e=1705536000&v=beta&t=mzAcbAw2BRmDwJ9H3SVf9f1_CTfoeyis9yV0aGm75y8"
+    alt="user"
+  />
+  <h3>Anupam Singh</h3>
+  <h6>Full stack Web developer</h6>
+  <p>
+  Stay informed about your transactions with real-time updates and notifications. Your financial journey is important to us, and we're committed to ensuring it's secure and convenient.
+  </p>
+  <div className="buttons">
+    <button className="primary">Message</button>
+ 
+  </div>
+  <div className="skills">
+    <h6>Skills</h6>
+    <ul>
+      <li>UI / UX</li>
+      <li>Front End Development</li>
+      <li>HTML</li>
+      <li>CSS</li>
+      <li>JavaScript</li>
+      <li>React</li>
+      <li>Node</li>
+      <li>Express.js</li>
+      <li>Mongodb</li>
+      
+    </ul>
+  </div>
+  </div>
+  </div>
+
+
+</div>
+
+    </>
+
+   
+  )
+}
+
+export default About
